833a6d8bf416cd7663d912ee78490fed80eac149
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2020-07-03 10:42:43 +1000

    updated

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, session, request, redirect, url_for, flash, jsonify
 import sqlite3, uuid, hashlib, sys, logging #other libraries
 from datetime import datetime
-from databaseinterface import DatabaseHelper
+from interfaces.databaseinterface import DatabaseHelper
 
 #--------------------------------------------------------------------------------------
 # SETTINGS for the Flask Web Application
 #-----------------------------------------------------------------------------------
-DEBUG = True #sets the level of logging
+DEBUG = True #sets the level of logging to high
 SECRET_KEY = 'my random key can be anything' #this random sequence is required to encrypt Sessions
 app = Flask(__name__) #Creates a handle for the Flask Web Server
 app.config.from_object(__name__) #Set app configuration using above SETTINGS
-database = DatabaseHelper('/home/nielbrad/mysite/test.sqlite')
+#database = DatabaseHelper('/home/nielbrad/mysite/test.sqlite') #on Python Anywhere
+database = DatabaseHelper('test.sqlite')
+database.set_log(app.logger) #set the logger inside the database
 
 #for encrypting the password in the database
 def hash_password(password):
@@ -64,27 +66,7 @@ def index():
         flash("No data submitted")
     return render_template('login.html')
 
-@app.route('/register', methods=['GET','POST'])
-def register():
-    data = None
-    if request.method == "POST":
-        name = request.form['name']   #get the form field with the name username
-        password = request.form['password']
-        passwordconfirm = request.form['passwordconfirm']
-        phone = request.form['phone']
-        age = request.form['age']
-        email = request.form['email']
-        if password != passwordconfirm:
-            flash("Passwords do no match!") #flash is a means of debugging on the web page before and after redirects
-            return render_template('register.html', data=data) #exit function
-        users = database.ViewQueryHelper("SELECT * FROM users WHERE email = ?",(email,)) #check that the username doesnt already exist
-        if (users != None):
-            flash("Username already exists!")
-            return render_template('register.html', data=data) #exit function
-        database.ModifyQueryHelper("INSERT INTO users (name, password, permission, email, phone, active) VALUES (?,?,?,?,?,?)",(name,password,'user',email,phone,1))
-        return redirect('./')
-    return render_template('register.html', data=data)
-
+#homepage is shown once user is logged in
 @app.route('/home', methods=['GET','POST'])
 def home():
     if 'userid' not in session:
@@ -93,36 +75,33 @@ def home():
     flash("Login successful")
     return render_template('home.html', data=data)
 
+#admin page only available to admin, redirects for anyone else
 @app.route('/admin', methods=['GET','POST'])
 def admin():
     data = None
-    if 'permission' in session:
+    if 'permission' in session: #check to see if session cookie contains the permission level
         if session['permission'] != 'admin':
             return redirect('./')
     else:
-        return redirect('/')
+        return redirect('/') #user has not logged in
     if request.method == "POST":
-        userids = request.form.getlist('delete')
+        userids = request.form.getlist('delete') #gets checkboxes, each checkbox contains the user id value
         for userid in userids:
             if int(userid) > 1: #make sure you can not delete the admin user
-                database.ModifyQueryHelper("DELETE FROM users WHERE userid = ?",(userid,))
+                database.ModifyQueryHelper("DELETE FROM users WHERE userid = ?",(userid,)) #note the use of comma after userid. A query must accept a list (tuple) of params, not just one param)
     userdetails = database.ViewQueryHelper("SELECT * FROM users")
     return render_template('admin.html', data=userdetails)
 
-#-------------JSON EXAMPLES------------------------------------
-@app.route('/defaultdatahandler', methods=['GET','POST'])
-def defaultdatahandler():
-    if request.method == 'POST':
-        var1 = request.form.get('var1')
-        var2 = request.form.get('var2')
-    return jsonify({"returnvar1":"value1","returnvar2":"value2"})
-
-#example of converting a table of results into JSON
-@app.route('/getallusers', methods=['GET','POST'])
-def getallusers():
-    results = database.ViewQueryHelper("SELECT username, lastaccess from users")
-    return jsonify([dict(row) for row in results]) #jsonify doesnt work with an SQLite.Row so must convert
-#-------------------------------------------------------------------------------------------
+#register a new user - activity for students - create a register page
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == "POST":
+        pass
+        #get all the form fields and store in variables
+        #if password does not equal password confirm return an error
+        #search database for email, if email is already in use return an error
+        #else insert new user into database and redirect to login page
+    return render_template('register.html')
 
 @app.route('/logoff')
 def logoff():
@@ -134,6 +113,28 @@ def log(message):
     app.logger.info(message)
     return
 
+#----ADVANCED CODING FOR YEAR 12s---------------------------------------
+#-------------JSON EXAMPLES---------------------------------------------
+@app.route('/defaultdatahandler', methods=['GET','POST'])
+def defaultdatahandler():
+    var1 = None; var2 = None;
+    if request.method == 'POST':
+        var1 = request.form.get('var1')
+        var2 = request.form.get('var2')
+    return jsonify({"returnvar1":var1,"returnvar2":var2})
+
+#example of converting a table of results into JSON
+@app.route('/getallusers', methods=['GET','POST'])
+def getallusers():
+    results = database.ViewQueryHelper("SELECT username, lastaccess from users")
+    if results:
+        return jsonify([dict(row) for row in results]) #jsonify doesnt work with an SQLite.Row so must convert to list
+    else:
+        return jsonify({"message":"No users found"}) #jsonigy converts a python dictionary to string encoding
+#-------------------------------------------------------------------------------------------
+
+
 #main method called web server application
 if __name__ == '__main__':
-    app.run()
+    #app.run() #on python anywhere you will be able to access your server through your web app link
+    app.run(host='0.0.0.0', port=5000) #runs on port 5000
