ea2339847656202ab1635a91842d97a53097c75f
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2020-09-13 21:19:23 +1000

    updated to include interfaces to brickpi & grovepi

---
+++ interfaces/grovepiinterface.py
@@ -0,0 +1,108 @@
+import grovepi
+import time
+import math
+import sys
+import logging
+import threading
+from di_sensors.easy_mutex import ifMutexAcquire, ifMutexRelease 
+from di_sensors.temp_hum_press import TempHumPress
+from grove_rgb_lcd import *
+
+class GrovePiInterface():
+
+    #Initialise log and timelimit (used to exit a function after time)
+    def __init__(self, timelimit=20):
+        self.logger = logging.getLogger()
+        self.CurrentCommand = "loading"
+        self.Configured = False #is the grove configured?
+
+    # This function will return the current light reading from the desired ANALOG port A0, A1 etc
+    def read_light_sensor_analogueport(self, port):
+        light_sensor = port
+        grovepi.pinMode(light_sensor,"INPUT")
+        sensor_value = None
+        try:
+            sensor_value = grovepi.analogRead(light_sensor) # Get sensor value
+        except IOError: #this doesnt appear to work
+            log.error("Error in reading the light sensor")
+        return sensor_value
+
+    # This function will return the current light reading from the desired ANALOG port A0, A1 
+    def read_ultra_digitalport(self, port):
+        ultra = port
+        grovepi.pinMode(ultra,"INPUT")
+        sensor_value = None
+        try:
+            sensor_value = grovepi.ultrasonicRead(ultra) # Get sensor value
+        except IOError: #this doesnt appear to work
+            log.error("Error in reading the ultra sensor")
+        return sensor_value
+
+    #Turn on the led
+    def turn_on_led_digitalport(self, port):
+        led = port
+        grovepi.pinMode(led,"OUTPUT")
+        grovepi.digitalWrite(led,255)
+        return
+
+    #Turn off the led
+    def turn_off_led_digitalport(self, port):
+        led = port
+        grovepi.pinMode(led,"OUTPUT")
+        grovepi.digitalWrite(led,0)
+        return
+
+    #read temp and humidity
+    def read_temp_humidity_sensor_digitalport(self, port):
+        tempsensor = port
+        grovepi.pinMode(tempsensor,"INPUT")
+        temp_humidity_list = None
+        try:
+            temp_humidity_list = grovepi.dht(port,0) #0 - type blue sensor
+        except IOError: #this doesnt appear to work
+            log.error("Error in reading the temp and humidity sensor")
+        return temp_humidity_list
+
+
+    '''moisture=grovepi.analogRead(mositure_sensor)
+	light=grovepi.analogRead(light_sensor)
+	[temp,humidity] = grovepi.dht(temp_himidity_sensor,white)
+		#Return -1 in case of bad temp/humidity sensor reading
+	if math.isnan(temp) or math.isnan(humidity):		#temp/humidity sensor sometimes gives nan
+			return [-1,-1,-1,-1]
+		return [moisture,light,temp,humidity]
+
+        pinMode(buzzer_pin,"OUTPUT")
+        digitalWrite(buzzer_pin,1)
+        digitalWrite(buzzer_pin,0)
+        
+        pinMode(button,"INPUT")		# Assign mode for Button as input
+        button_status= digitalRead(button)
+    '''
+
+    # display the ipaddress on the RGB display if connect
+    def run_ipaddress_RGB_display(self):
+        setRGB(0,128,64)
+        setText(get_ip() + "  " + get_macaddress()) #sets the text of the LCD
+        elapsedtime = 0
+        starttime = time.time()
+        c = 1
+        diff = 1
+        # Slowly change the colors every 0.01 seconds.
+        while elapsedtime < 120:
+            elapsedtime = time.time() - starttime
+            setRGB(c,255-c,0)
+            if c == 255 or c==0:
+                diff = -diff
+            c+=diff  
+            time.sleep(0.1)
+        setRGB(0,0,0) #not sure how to turn off
+        setText("")
+        return
+
+# Only execute if this is the main file, good for testing code
+if __name__ == '__main__':
+    grove = GrovePiInterface(timelimit=20)
+    logger = logging.getLogger()
+    logger.setLevel(logging.info)
+    grove.set_log(logger)
