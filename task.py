f7be798e83304838a47429c57e60b69e48452a56
bradnielsen2981 <38126504+bradnielsen2981@users.noreply.github.com>
2020-10-12 11:23:51 +1000

    commit

--- task.py
+++ task.py
@@ -1,34 +1,33 @@
 import grovepi
+import time
 
 ENABLED = True
-GROVEPILIGHTSWITCH = False
+grovepilightswitch = False
 
 # Turn on the led using digital port 
-def switch_led_digitalport_value(port, value=255):
+def switch_led_digitalport_value(port, value=1):
     if not ENABLED:
         return -1
-    global GROVEPILIGHTSWITCH
+    global grovepilightswitch
     grovepi.pinMode(port,"OUTPUT") #should be in initialise
-    if GROVEPILIGHTSWITCH:
+    if grovepilightswitch:
         grovepi.digitalWrite(port,0)
-        GROVEPILIGHTSWITCH = False
+        grovepilightswitch = False
     else:
         grovepi.digitalWrite(port,value)
-        GROVEPILIGHTSWITCH = True
+        grovepilightswitch = True
     return
 
 # Read temp and humidity
 def read_temp_humidity_sensor_digitalport(port):
     if not ENABLED:
         return -1
-    grovepi.pinMode(tempsensor,"INPUT")
     temp_humidity_list = None
     try:
-        temp_humidity_list = grovepi.dht(port,0) #0 - type blue sensor
+        temp_humidity_list = grovepi.dht(port,0)
     except IOError: #this doesnt appear to work
         print("Error in reading the temp and humidity sensor")
     return temp_humidity_list
 
-#turn on the light on digital port 2, 255
-switch_led_digitalport_value(2)
-print("Temperature and Humidity: " + str(read_temp_humidity_sensor_digitalport(3)))
+print(read_temp_humidity_sensor_digitalport(2))
+switch_led_digitalport_value(4,1)
